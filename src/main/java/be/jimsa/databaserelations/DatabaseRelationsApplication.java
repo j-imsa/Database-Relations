57005d137ed6432c0aa620f39bcde065fb31fb84
Iman Salehi <cse.isalehi@gmail.com>
2024-08-17 22:06:47 +0330

    init proj

---
+++ src/main/java/be/jimsa/databaserelations/DatabaseRelationsApplication.java
@@ -0,0 +1,13 @@
+package be.jimsa.databaserelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseRelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseRelationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/jimsa/databaserelations/DatabaseRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jimsa.databaserelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseRelationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
